1cea1cb46a27a699beabb254dd49e756d2722fa6
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-02-16 12:31:25 -0800

    Unix is simple

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 
 /**
  * main - UNIX is basically a simple operating system
@@ -8,8 +9,16 @@
  */
 int main(void)
 {
-	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	ssize_t len = sizeof(message) - 1;
+	char message1[] = "and that piece of art is useful\" - ";
+	char message2[] = "Dora Korpar, 2015-10-19\n";
+	char message[128];
+	ssize_t len;
+
+	strcpy(message, message1);
+	strcat(message, message2);
+	len = strlen(message);
+
 	write(STDERR_FILENO, message, len);
+
 	return (1);
 }
