f679a1e8e1c3c5f56272fb7028769e3c0e498bea
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-02-17 10:27:41 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - i sometimes suffer from insomnia
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+	printf("%d", number);
+	}
+	putchar('\n');
+
+	return (0);
+}
