681531605f2a49afa24b2806fe7ba86d8f02f1d1
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-02-17 11:06:49 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - i sometimes suffer from insomnia
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z'; alpha >= 'a'; alpha--)
+	{
+	putchar(alpha);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
