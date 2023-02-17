d8f64257222e5fb0060bd4e6333f374e65b0b077
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-02-17 11:57:53 -0800

    patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - i sometimes suffer from insomnia
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+	int digit;
+	int divisor;
+
+	divisor = 1;
+	while (number / divisor >= 10)
+	{
+	divisor *= 10;
+	}
+	while (divisor > 0)
+	{
+	digit = (number / divisor) % 10;
+	putchar('0' + digit);
+	if (divisor > 9)
+	{
+	}
+	divisor /= 10;
+	}
+	putchar((number < 9) ? ',' : '$');
+	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
