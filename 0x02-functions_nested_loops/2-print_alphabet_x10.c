c438e39b2629b84680ee0a23c36834a2daec7b51
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-02-21 14:55:05 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_10_times - prints all alphabets in lowercase 10 times.
+ *
+ * Return: 0 (Success)
+ *
+ */
+void print_alphabet_10_times(void)
+{
+	int i;
+	char j;
+
+	for (i = 0; i < 10; i++)
+	{
+		j = 'a';
+		while (j <= 'z')
+		{
+			putchar(j);
+			j++;
+		}
+		putchar('\n');
+	}
+}
