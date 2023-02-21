ecd1560214c91d4792704c5b572a2a2ef0bb251f
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-02-21 14:29:12 -0800

    I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabets in lowercase
+ *
+ * Return:0 (Success)
+ *
+ */
+
+	void print_alphabet(void)
+	{
+		char i;
+
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			putchar(i);
+		}
+		putchar('\n');
+	}
+	return (0);
