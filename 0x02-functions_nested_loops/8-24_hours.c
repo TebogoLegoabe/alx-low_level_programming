e40370dc5bfc16274fa9f4caaf3ce3fae4b176af
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-02-22 11:55:04 -0800

    8. I'm federal agent Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,31 +5,21 @@
 *
 * Return:0 if succeesful.
 */
+
+int h, m;
+
 void jack_bauer(void)
 {
-
-int i, j, k, m;
-
-for (i = 0; i <= 2; i++)
+for (h = 0; h < 24; h++)
 {
-for (j = 0; j <= 9; j++)
+for (m = 0; m < 60; m++)
 {
-for (k = 0; k <= 5; k++)
-{
-for (m = 0; m <= 9; m++)
-{
-if (i == 2 && j > 3)
-{
-    break;
-}
-_putchar(i + '0');
-_putchar(j + '0');
+_putchar(h / 10 + '0');
+_putchar(h % 10 + '0');
 _putchar(':');
-_putchar(k + '0');
-_putchar(m + '0');
+_putchar(m / 10 + '0');
+_putchar(m % 10 + '0');
 _putchar('\n');
-}
-}
 }
 }
 }
