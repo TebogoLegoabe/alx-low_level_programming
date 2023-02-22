109f266b85b73fd4cc47f9eb177a72c8ca45a904
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-02-22 10:21:57 -0800

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints all alphabets in lowercase 10 times.
+*
+* Return: On success 1
+*/
+
+void print_alphabet_x10(void)
+{
+char j; 
+
+for (j = 0; j < 10; j++)
+{
+	char i;
+
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
+}
