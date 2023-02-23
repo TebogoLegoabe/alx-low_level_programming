d2ee3b3010e3366670e2c9d01f5a005e59a0d954
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-02-23 05:12:16 -0800

    0. Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,14 +2,16 @@
 
 /**
 * main - Test function for positive or negative
-*
-* Return: 0
+* Return: 0 (Success)
 */
 int main(void)
 {
+
 int i;
 
 i = 0;
+
 positive_or_negative(i);
+
 return (0);
 }
