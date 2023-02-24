b48d730cb72506582cf4e50d0ef89245aaf0c0b8
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-02-23 16:05:43 -0800

    3. The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,16 @@
 
 /**
  * print_numbers - print 0-9
- * return: void
+ * Return: The numbers from 0 to 9
  */
 
 void print_numbers(void)
 {
-char c;
+int i;
 
-for (c = '0'; c <= '9'; c++)
+for (i = 0; i <= 9; i++)
 {
-_putchar(c);
+_putchar(i + '0');
 }
 _putchar('\n');
 }
