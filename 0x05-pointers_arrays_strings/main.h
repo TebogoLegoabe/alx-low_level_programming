2d85acff687ca2e40bba875b12a4a83ee1ca2d08
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-02-27 12:51:16 -0800

    2. This report, by its very length

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,7 +5,7 @@
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 void _putchar(char c);
-void _puts(const char *str);
+void _puts(char *str);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
