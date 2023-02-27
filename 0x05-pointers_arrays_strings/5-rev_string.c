df5e18c46a29d88a8243ad5748eec9a6d5c18a8f
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-02-27 12:03:51 -0800

    5. A good engineer thinks in reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,16 +8,16 @@
  */
 void rev_string(char *s)
 {
-	int len = 0, index = 0;
+	int length = 0, index = 0;
 	char tmp;
 
 	while (s[index++])
-		lrn++;
+		length++;
 
-	for (index = len - 1; index >= len / 2; index--)
+	for (index = length - 1; index >= len / 2; index--)
 	{
 		tmp = s[index];
-		s[index] = s[len - index -1];
-		s[len - index - 1] = tmp;
+		s[index] = s[length - index -1];
+		s[length - index - 1] = tmp;
 	}
 }
