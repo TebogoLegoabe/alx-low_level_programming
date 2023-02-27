61c2251d1baeb989061f7bf6ddc6e8fa38b634b6
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-02-27 09:45:48 -0800

    1. Don't swap horses in crossing a stream

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,8 +3,9 @@
 /**
  * swap_int - swap two integers
  *
- * @a - value to be swapped
- * @b - value to swap
+ * @a: - value to be swapped
+ * @b: - value to swap
+ * This function swaps the values of the two integers pointed by a and b
  *
  * Return: 0 (Success)
  */
