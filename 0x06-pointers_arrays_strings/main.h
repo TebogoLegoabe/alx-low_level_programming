303ad5592cbf032df9181a11a454aee1eb7069d7
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-03-01 15:42:14 -0800

    10. A dream doesn't become reality through magic

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,6 +10,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *str);
 char *leet(char *str);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
 #endif
