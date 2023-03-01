0dfc1253d92c02a5cba8e611798b8552115fbe1d
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-03-01 15:22:37 -0800

    8. rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,34 +1,44 @@
 #include "main.h"
 
 /**
- * print_number - print numbers chars
- * @n: integer params
+ * rot13 - encoder rot13
+ * @str: pointer to params string
  *
- * Return: 0 (Success)
+ * Return: ptr
  */
 char *rot13(char *str) {
-char *ptr = str;
-int i, j;
+char *ptr;
+int i;
+int j;
 
-for (i = 0; str[i]; i++) {
-if (str[i] >= 'a' && str[i] <= 'z') {
-for (j = 0; j < 13; j++) {
-if (str[i] == 'z') {
+*ptr = str;
+
+for (i = 0; str[i]; i++)
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+for (j = 0; j < 13; j++)
+{
+if (str[i] == 'z')
+{
 str[i] = 'a';
 } else {
 str[i]++;
 }
 }
-} else if (str[i] >= 'A' && str[i] <= 'Z') {
-for (j = 0; j < 13; j++) {
-if (str[i] == 'Z') {
+} else if (str[i] >= 'A' && str[i] <= 'Z')
+{
+for (j = 0; j < 13; j++)
+{
+if (str[i] == 'Z')
+{
 str[i] = 'A';
-} else {
+} else
+{
 str[i]++;
 }
 }
 }
 }
-
 return (ptr);
 }
