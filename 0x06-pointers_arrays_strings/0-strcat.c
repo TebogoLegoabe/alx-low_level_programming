5cde8902f921637eb83ca6396ef6965d1654ebd3
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-03-01 03:00:18 -0800

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: string
+ * @src: src string to appends
+ *
+ * Return: a pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ptr;
+       
+	*ptr = dest + strlen(dest);
+	while (*src != '\0')
+	{
+		*ptr++ = *src++;
+	}
+	*ptr = '\0';
+	return (dust);
+}
