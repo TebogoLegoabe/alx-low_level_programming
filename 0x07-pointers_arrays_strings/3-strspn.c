0d00207ae6c278d29643db2aa01805841031655d
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-03-06 14:28:10 -0800

    4. strpbrk

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: input string
+ * @accept: input string containing the characters to match
+ *
+ * Return: number of characters in the initial segment of s that are in accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n = 0;
+
+	while (*s && strchr(accept, *s))
+	{
+	n++;
+	s++;
+	}
+	return (n);
+}
