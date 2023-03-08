ad3689bd029d8e58b7d64acf2d9b97c6c75be7b9
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-03-08 09:46:48 -0800

    2. Dreams feel real while we're in them

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns length of string
+ * @s: string
+ *
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+	return (0);
+	}
+	else
+	return (1 + _strlen_recursion(s + 1));
+}
