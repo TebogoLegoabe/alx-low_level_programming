57b994482321317624554b223ff459763724792f
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-03-08 15:19:21 -0800

    7. They say we only use a fraction of our brain's true potential

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,28 +1,27 @@
 #include "main.h"
 
-int is_palindrome(char *s);
+int check_pal(char *s, int i, int j);
 int _strlen_recursion(char *s);
 
 /**
  * is_palindrome - checks if a string is a palindrome
- * @s: the string to check
+ * @s: string to check
  *
- * Return: 1 if the string is a palindrome, 0 otherwise
+ * Return: 1 if it is a palindrome, 0 otherwise
  */
 int is_palindrome(char *s)
 {
 if (*s == '\0')
 return (1);
 
-int len = _strlen_recursion(s);
-return check_palindrome(s, 0, len - 1);
+return (check_pal(s, 0, _strlen_recursion(s)));
 }
 
 /**
 * _strlen_recursion - returns the length of a string
-* @s: the string to get the length of
+* @s: string to calculate the length of
 *
-* Return: the length of the string
+* Return: length of the string
 */
 int _strlen_recursion(char *s)
 {
@@ -33,20 +32,20 @@ return (1 + _strlen_recursion(s + 1));
 }
 
 /**
-* check_palindrome - recursively checks if a string is a palindrome
-* @s: the string to check
-* @start: the starting index of the string
-* @end: the ending index of the string
+* check_pal - recursively checks if a string is a palindrome
+* @s: string to check
+* @i: starting index of the string
+* @j: ending index of the string
 *
-* Return: 1 if the string is a palindrome, 0 otherwise
+* Return: 1 if it is a palindrome, 0 otherwise
 */
-int check_palindrome(char *s, int start, int end)
+int check_pal(char *s, int i, int j)
 {
-if (start >= end)
-return (1);
-
-if (s[start] != s[end])
+if (s[i] != s[j])
 return (0);
 
-return (check_palindrome(s, start + 1, end - 1));
+if (i >= j)
+return (1);
+
+return (check_pal(s, i + 1, j - 1));
 }
