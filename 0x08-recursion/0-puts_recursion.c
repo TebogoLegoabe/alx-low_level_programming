f31a4c3be118cafe0646916f09e2cdb2a59c0b34
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-03-08 09:17:26 -0800

    0. She locked away a secret

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by new line
+ * @s: string parameter
+ *
+ * Return: 0 (Success)
+ */
+void _puts_recursion(char *s)
+{
+	void _puts_recursion(char *s)
+	{
+		if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+		_putchar(*s);
+	_puts_recursion(s + 1);
+	}
