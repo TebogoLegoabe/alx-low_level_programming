645458211be81b4c609b39bdc3b73d1452ef3e3f
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-03-13 16:57:14 -0700

    6. I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,44 +1,97 @@
-#include "main.h"
 #include <stdlib.h>
-#include <string.h>
+#include "main.h"
 
 /**
- * **strtow - splits a string into words
- * @str: string
+ * count_words - helper function to count the number of words in a string
+ * @str: string to evaluate
  *
- * Return: a pointer to an array of strings or
- * NULL if str == NULL or str == ""
+ * Return: number of words
  */
+int count_words(char *str)
+{
+int flag, count, i;
+
+flag = 0;
+count = 0;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] == ' ')
+{
+flag = 0;
+}
+else if (flag == 0)
+{
+flag = 1;
+count++;
+}
+}
+return (count);
+}
+
+/**
+* strtow - splits a string into words
+* @str: string to split
+*
+* Return: pointer to an array of strings (Success)
+*         or NULL (Error)
+*/
 char **strtow(char *str)
 {
-	char **words;
-	int word_count;
-	char *test;
-	char *spt;
+char **words_array, *temp_word;
+int i, string_length, word_count, start, end;
 
-	if (str == NULL || *str == '\0')
-	{
-		return (NULL);
-	}
-	words = malloc(sizeof(char *));
-	if (words == NULL)
-	{
-		return (NULL);
-	}
-	word_count = 0;
-	test = strtok(str, " ");
-	while (test != NULL)
-	{
-		spt = realloc(words, (word_count + 2) * sizeof(char *));
-		if (spt == NULL)
-		{
-			free(words);
-			return (NULL);
-		}
-		words = &spt;
-		words[word_count++] = strdup(test);
-		test = strtok(NULL, " ");
-	}
-	words[word_count] = NULL;
-	return (words);
+tmp = 0;
+string_length = 0;
+word_count = 0;
+word_length = 0;
+start = 0;
+end = 0;
+
+while (*(str + string_length))
+{
+string_length++;
+}
+word_count = count_words(str);
+
+if (word_count == 0)
+{
+return (NULL);
+}
+words_array = (char **) malloc(sizeof(char *) * (word_count + 1));
+if (words_array == NULL)
+{
+return (NULL);
+}
+for (i = 0; i <= string_length; i++)
+{
+if (str[i] == ' ' || str[i] == '\0')
+{
+if (word_length)
+{
+end = i;
+temp_word = (char *) malloc(sizeof(char) * (word_length + 1));
+if (temp_word == NULL)
+{
+return (NULL);
+}
+while (start < end)
+{
+*temp_word++ = str[start++];
+}
+
+*temp_word = '\0';
+
+words_array[tmp] = temp_word - word_length;
+tmp++;
+word_length = 0;
+}
+}
+else if (word_length++ == 0)
+{
+start = i;
+}
+}
+words_array[tmp] = NULL;
+return (words_array);
 }
