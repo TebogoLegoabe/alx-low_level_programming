113b12f29b537cf4e3083b30b70b2f4ca9c31b2e
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-03-13 07:15:11 -0700

    0. Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,10 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	char *array = (char *)malloc(size *sizeof(char));
+	
+	char *array;
+
+	array = (char *)malloc(size *sizeof(char));
 	if (array == NULL)
 	{
 		return (NULL);
