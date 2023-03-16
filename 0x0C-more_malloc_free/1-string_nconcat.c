7282db361c49541cb69b1e09256278688661d65e
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-03-16 09:41:19 -0700

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,27 +22,37 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	length_one = 0;
 	length_two = 0;
 	while (s1 && s1[length_one])
+	{
 		length_one++;
+	}
 	while (s2 && s2[length_two])
+	{
 		length_two++;
+	}
 	if (n < length_two)
+	{
 		c_string = malloc(sizeof(char) * (length_one + n + 1));
+	}
 	else
 		c_string = malloc(sizeof(char) * (length_one + length_two + 1));
 
 	if (!c_string)
+	{
 		return (NULL);
-
+	}
 	while (i < length_one)
 	{
 		c_string[i] = s1[i];
 		i++;
 	}
 	while (n < length_two && i < (length_one + n))
+	{
 		c_string[i++] = s2[j++];
+	}
 	while (n >= length_two && i < (length_one + length_two))
+	{
 		c_string[i++] = s2[j++];
 		c_string[i] = '\0';
-
+	}
 	return (c_string);
 }
