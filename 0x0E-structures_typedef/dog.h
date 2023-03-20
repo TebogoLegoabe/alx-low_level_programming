5b1872ea009a0b21d4d04d84266c78e347e0431a
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-03-20 09:35:36 -0700

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,7 +19,7 @@ struct dog
 /**
  * dog_t - typedef of struct dog
  */
-typedef_t struct dog dog_t;
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
