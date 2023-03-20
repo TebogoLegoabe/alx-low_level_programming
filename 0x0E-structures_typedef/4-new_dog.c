9af2488c121bd34684ec83c26ad438bd53b1b493
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-03-20 10:19:11 -0700

    5. How many legs does a dog have if you call his tail a leg? Four.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -62,8 +62,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dog;
 	int len1, len2;
 
-	len1 = _strlen(name);
-	len2 = _strlen(owner);
+	len1 = _strlen(name);	len2 = _strlen(owner);
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
