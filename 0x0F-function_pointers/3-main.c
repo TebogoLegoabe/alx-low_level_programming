318aace302ddcf2b3968ecae89349f296f152361
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-03-22 16:47:05 -0700

    3. A goal is not always meant to be reached

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,40 +1,43 @@
-#include "function_pointers.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "3-calc.h"
+
 /**
- * main - Prints the result of simple operations.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
  *
- * Return: 0 (Success)
+ * Return: Always 0 (Success)
  */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int num1, num2;
-	char *ptr;
+int num1, num2, result;
+int (*op_func)(int, int);
 
-		if (argc != 4)
-		{
-			printf("Error\n");
-			exit(98);
-		}
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
 
-	num1 = atoi(argv[1]);
-	ptr = argv[2];
-	num2 = atoi(argv[3]);
-	if (get_op_func(ptr) == NULL || op[1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	if ((*op == '/' && num2 == 0) ||
-	(*ptr == '%' && num2 == 0))
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	printf("%d\n", get_op_func(op)(num1, num2));
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+op_func = get_op_func(argv[2]);
 
-	return (0);
+if (op_func == NULL)
+{
+printf("Error\n");
+exit(99);
+}
+
+if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+{
+printf("Error\n");
+exit(100);
+}
+
+result = op_func(num1, num2);
+printf("%d\n", result);
+
+return (0);
 }
