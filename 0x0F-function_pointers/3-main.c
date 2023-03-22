1abb089421f5dac10c4ba365eb1ec24c4636fd51
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-03-22 16:40:24 -0700

    3. A goal is not always meant to be reached

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 (Success)
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	int num1, num2;
+	char *ptr;
+
+		if (argc != 4)
+		{
+			printf("Error\n");
+			exit(98);
+		}
+
+	num1 = atoi(argv[1]);
+	ptr = argv[2];
+	num2 = atoi(argv[3]);
+	if (get_op_func(ptr) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*op == '/' && num2 == 0) ||
+	(*ptr == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(op)(num1, num2));
+
+	return (0);
+}
