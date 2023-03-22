90cbedf5c8fbe0b470eb6a595114517f202f94c8
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-03-22 14:55:55 -0700

    2. To hell with circumstances; I create opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,9 +15,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if (size <= 0 || array == NULL || cmp == NULL)
-	{
 		return (-1);
-	}
+	
 	for (int i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
