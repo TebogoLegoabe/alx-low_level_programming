b9bb674b453689891650c1da22d2938bb4ec1261
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-03-22 17:45:06 -0700

    4. Most hackers are young because young people tend to be adaptable

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 /**
- * main - prints its own opcodes
+ * main - prints its own opcodes of its own main function
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: Always 0 (Success)
+ * Return: 0 (Success)
  */
 int main(int argc, char *argv[])
 {
