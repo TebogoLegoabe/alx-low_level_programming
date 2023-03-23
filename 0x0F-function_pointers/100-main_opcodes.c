fddf0a873190e437c5f787b32c51c3240f5e1f8d
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-03-22 17:43:26 -0700

    4. Most hackers are young because young people tend to be adaptable

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -3,35 +3,40 @@
 
 /**
  * main - prints its own opcodes
- * @argc: number of bytes to print
- * @argv: pointer to the start of the memory block to print
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	int num_bytes, i;
-	char *ptr;
+int num_bytes, i;
+char *ptr;
 
-	if (argc != 2)
-	{
-	printf("Error\n");
-	exit(1);
-	num_bytes = atoi(argv[1]);
-	if (num_bytes < 0)
-	{
-	printf("Error\n");
-	exit(2);
-	}
-	ptr = (char *)main;
-	for (i = 0; i < num_bytes; i++)
-	{
-	if (i == num_bytes - 1)
-	{
-	printf("%02hhx\n", ptr[i]);
-	break;
-	}
-	printf("%02hhx ", ptr[i]);
-	}
-	return (0);
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+
+num_bytes = atoi(argv[1]);
+
+if (num_bytes < 0)
+{
+printf("Error\n");
+exit(2);
+}
+
+ptr = (char *)main;
+
+for (i = 0; i < num_bytes; i++)
+{
+if (i == num_bytes - 1)
+{
+printf("%02hhx\n", ptr[i]);
+break;
+}
+printf("%02hhx ", ptr[i]);
+}
+return (0);
 }
