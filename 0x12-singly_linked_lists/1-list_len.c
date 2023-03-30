53cf64b1cee3562cc339ad7fe5eb9471bdef4930
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-03-30 05:35:25 -0700

    1. List length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * list_len -  returns the number of elements in a linked list_t list
+ * @h: pointer
+ *
+ * Return: number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count;
+
+	count = 0;
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
