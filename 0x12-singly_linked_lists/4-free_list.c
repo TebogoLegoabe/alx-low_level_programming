712152a3adffb341c12aa4d5a6118489f715982b
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-03-30 06:18:18 -0700

    4. Free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -3,17 +3,17 @@
 
 /**
  * free_list - frees a linked list
- * @head: pointer to the head of the list
+ * @head: pointer to the end of the list
  */
 void free_list(list_t *head)
 {
-	list_t *temp;
+list_t *temp;
 
-	while (head)
-	{
-	temp = head->next;
-	free(temp->str);
-	free(head);
-	head = temp;
-	}
+while (head)
+{
+temp = head->next;
+free(head->str);
+free(head);
+head = temp;
+}
 }
