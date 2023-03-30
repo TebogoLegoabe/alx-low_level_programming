4638a74b92f335e007e55eb312ab1b30ecd18fc1
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-03-30 06:46:36 -0700

    5. The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void first(void);
+void init(void) __attribute__((constructor));
+
+void init(void) {
+  first();
+}
+
+
+void first(void) {
+  printf("You're beat! and yet, you must allow,\n");
+  printf("I bore my house upon my back!\n");
+}
