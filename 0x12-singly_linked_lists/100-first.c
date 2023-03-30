b4d9ec489b7645c53360fea65794a1824abe3390
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-03-30 07:00:43 -0700

    5. The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
+/**
+ * first - Prints a string before the main function is called
+ *
+ * This function is called by the init function before
+ * the main function is called.
+ *
+ * Return: 0 (Success)
+ */
+
 void first(void);
+
+/**
+ * init - Initializes the program by calling the first function.
+ *
+ * This function is marked with the constructor attribute
+ * which causes it to be executed
+ * before the main function is called.
+ * It calls the first function to print a message to the console.
+ */
+
 void init(void) __attribute__((constructor));
 
 void init(void)
