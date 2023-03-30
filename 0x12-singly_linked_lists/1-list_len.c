ae5468d76599da9c5c54a7f74267241813163a47
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-03-30 05:49:53 -0700

    1. List length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,12 +9,12 @@
  */
 size_t list_len(const list_t *h)
 {
- size_t count = 0;
+	size_t count = 0;
 
- while (h)
- {
- count++;
- h = h->next;
- }
- return (count);
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
 }
