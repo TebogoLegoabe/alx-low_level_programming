5ee7c83945329fcb0ca280deeab89069d23dac6d
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-03-30 05:08:02 -0700

    0. Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,21 +3,6 @@
 #include <string>
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- */
-typedef struct list_s
-{
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
-
-/**
  * print_list - prints all the elements of a list_t list
  * @h: pointer
  *
