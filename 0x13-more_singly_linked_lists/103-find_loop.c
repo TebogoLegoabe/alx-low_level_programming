651c2822f30c70edf2a73024158d0bd24c95a250
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-04-05 11:05:37 -0700

    14. Find the loop

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,37 +1,34 @@
 #include "lists.h"
 
 /**
- * find_listint_loop - function that finds the loop in a linked list
- * @head: A pointer to the head
+ * find_listint_loop - finds the loop in a linked list
+ * @head: linked list to search for
  *
- * Return: If there is no loop - NULL
+ * Return: address of the node where the loop starts, or NULL
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-listint_t *tmp, *hare;
+	listint_t *slow = head;
+	listint_t *fast = head;
 
-if (head == NULL || head->next == NULL)
-return (NULL);
+	if (!head)
+		return (NULL);
 
-tmp = head->next;
-hare = (head->next)->next;
+	while (slow && fast && fast->next)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+		if (fast == slow)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (fast);
+		}
+	}
 
-while (hare)
-{
-if (tmp == hare)
-{
-tmp = head;
-
-while (tmp != hare)
-{
-tmp = tortoise->next;
-hare = hare->next;
-}
-
-return (tmp);
-}
-tmp = tmp->next;
-hare = (hare->next)->next;
-}
-return (NULL);
+	return (NULL);
 }
