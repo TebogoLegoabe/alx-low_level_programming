1df576b88df0b872fd68a9ca0c72f9a9c83922cb
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-07-26 14:35:13 -0700

    updated

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,21 +13,14 @@ def island_perimeter(grid):
   Grid is completely surrounded by water, and there is one island (or nothing).
   The island doesn't have “lakes” (water inside that isn’t connected to the water around the island).
   """
-     perimeter = 0
+
+  perimeter = 0
   for i in range(len(grid)):
     for j in range(len(grid[0])):
       if grid[i][j] == 1:
         perimeter += 4
-        if i > 0 and grid[i - 1][j] == 1:
+        if i == 0 or grid[i - 1][j] == 0:
           perimeter -= 1
-        if j > 0 and grid[i][j - 1] == 1:
+        if j == 0 or grid[i][j - 1] == 0:
           perimeter -= 1
   return perimeter
-
-
-if __name__ == "__main__":
-  grid = [[0, 1, 0, 0],
-           [1, 1, 1, 0],
-           [0, 1, 0, 0],
-           [0, 0, 0, 0]]
-  print(island_perimeter(grid))
