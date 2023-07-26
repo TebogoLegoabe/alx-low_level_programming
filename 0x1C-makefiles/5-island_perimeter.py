05b6d06ccf8ef26606ab5aa128705c4c4a34104d
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-07-26 15:01:05 -0700

    updated

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,34 +1,27 @@
 #!/usr/bin/python3
 
 def island_perimeter(grid):
-  """
-  Returns the perimeter of the island described in grid.
+    """
+    
+    """
+    if not grid or not grid[0]:
+        return 0
 
-  grid is a list of list of integers:
-    0 represents a water zone
-    1 represents a land zone
-  One cell is a square with side length 1
-  Grid cells are connected horizontally/vertically (not diagonally).
-  Grid is rectangular, width and height don't exceed 100
-  Grid is completely surrounded by water, and there is one island (or nothing).
-  The island doesn't have “lakes” (water inside that isn’t connected to the water around the island).
-  """
+    rows = len(grid)
+    cols = len(grid[0])
+    perimeter = 0
 
-  perimeter = 0
-  for i in range(len(grid)):
-    for j in range(len(grid[0])):
-      if grid[i][j] == 1:
-        perimeter += 4
-        if i > 0 and grid[i - 1][j] == 1:
-          perimeter -= 1
-        if j > 0 and grid[i][j - 1] == 1:
-          perimeter -= 1
-  return perimeter
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 1
+                if j < cols - 1 and grid[i][j + 1] == 1:
+                    perimeter -= 1
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 1
+                if i < rows - 1 and grid[i + 1][j] == 1:
+                    perimeter -= 1
 
-
-if __name__ == "__main__":
-  grid = [[0, 1, 0, 0],
-           [1, 1, 1, 0],
-           [0, 1, 0, 0],
-           [0, 0, 0, 0]]
-  print(island_perimeter(grid))
+    return perimeter
